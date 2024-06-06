8454558a108a358aae43957a6f1c863402ca441e
Kevin Gallegos <kevingallegos2003@gmail.com>
2024-06-06 14:26:46 -0700

    Added shooting mechanic with placeholder shootsprite

--- src/Scenes/Platformer.js
+++ src/Scenes/Platformer.js
@@ -11,9 +11,16 @@ class Platformer extends Phaser.Scene {
         this.JUMP_VELOCITY = -550;
         this.PARTICLE_VELOCITY = 20;
         this.SCALE = 0.7;
+        //------Bullet variables-----
+        this.bulletSpeed = 2;
+        my.sprite.bullet = [];
+        my.sprite.lbullet = [];
+        this.space = null;
+        this.playerfliped = false;//Used to know which way to shoot bullet at directions based on which way player faces
     }
 
     create() {
+        this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.map = this.add.tilemap("platformer", 16, 16, 40, 20);
         this.tileset = this.map.addTilesetImage("platformer_tilemap", "tilemap_tiles");
         //ground layer
@@ -37,13 +44,39 @@ class Platformer extends Phaser.Scene {
     }
 
     update() {
+        console.log(my.sprite.lbullet.length);
+
+        //bullet creation and movement
+        if(Phaser.Input.Keyboard.JustDown(this.space)){
+            //console.log("urmom");
+            if(!this.playerfliped){
+                my.sprite.bullet.push(this.add.sprite(
+                    my.sprite.player.x, my.sprite.player.y-(my.sprite.player.displayHeight/2), "player","tile_0000.png")
+                );
+            }
+            else{
+                my.sprite.lbullet.push(this.add.sprite(
+                    my.sprite.player.x, my.sprite.player.y-(my.sprite.player.displayHeight/2), "player","tile_0000.png")
+                );
+            }
+        }
+        for (let bullet of my.sprite.bullet) {//right bullets
+            bullet.x += this.bulletSpeed;
+        }
+        for (let bullet of my.sprite.lbullet) {//left bullets
+            bullet.x -= this.bulletSpeed;
+        }
+        my.sprite.bullet = my.sprite.bullet.filter((bullet) => bullet.x < 750);//bullet removal
+        my.sprite.lbullet = my.sprite.lbullet.filter((bullet) => bullet.x > -10);//bullet removal
         //moving left and right controls
         if(cursors.left.isDown) {
             my.sprite.player.body.setAccelerationX(-this.ACCELERATION);
             my.sprite.player.setFlip(true, false);
+            this.playerfliped = true;
         } else if(cursors.right.isDown) {
             my.sprite.player.body.setAccelerationX(this.ACCELERATION);
             my.sprite.player.resetFlip();
+            this.playerfliped = false;
         } else {
             my.sprite.player.body.setAccelerationX(0);
             my.sprite.player.body.setDragX(this.DRAG);
